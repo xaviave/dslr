954b0d8b51ea1e3c8302b3c7d09d678786bed7aa
Louis TOUSSAINT <louistoussaint98@gmail.com>
2020-10-15 15:36:34 +0200

    Start histogram and annexes

--- sources/histogram.py
+++ sources/histogram.py
@@ -2,5 +2,8 @@ import os
 
 # Quel cours de Poudlard a une repartition des notes homogenes entre les quatres maisons ?
 
+# think about "mugging" ? google.ml.doc
+# think about generic data picker for homogenous
+# just print histogram
 if "__main__" == __name__:
-    os.system("python3 sources/main.py spe_visualiser -hh")
+    os.system("python3 sources/main.py -hh")
