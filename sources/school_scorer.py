90e967108b8c5289470416eac295ff06ae72da21
Louis TOUSSAINT <louistoussaint98@gmail.com>
2020-10-16 19:47:13 +0200

    black -l 100 sources/

--- sources/school_scorer.py
+++ sources/school_scorer.py
@@ -8,9 +8,13 @@ logging.getLogger().setLevel(logging.INFO)
 
 def school_scorer():
     try:
-        school_data = pd.read_csv(f"{os.path.abspath('dataset_truth.csv')}")["Hogwarts House"].values
+        school_data = pd.read_csv(f"{os.path.abspath('dataset_truth.csv')}")[
+            "Hogwarts House"
+        ].values
         user_data = pd.read_csv(f"{os.path.abspath('houses.csv')}")["Hogwarts House"].values
-        compared_data = sum([1 for i in range(1, len(school_data)) if school_data[i] == user_data[i]])
+        compared_data = sum(
+            [1 for i in range(1, len(school_data)) if school_data[i] == user_data[i]]
+        )
         return compared_data / len(school_data)
     except Exception as e:
         logging.error(e)
