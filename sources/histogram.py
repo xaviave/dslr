230f5f23225f914a723d2fe13a0f155b6d16e639
Louis TOUSSAINT <louistoussaint98@gmail.com>
2020-10-18 17:37:48 +0200

    Delete comment in histogram.py

--- sources/histogram.py
+++ sources/histogram.py
@@ -2,8 +2,5 @@ import os
 
 # Quel cours de Poudlard a une repartition des notes homogenes entre les quatres maisons ?
 
-# think about "mugging" ? google.ml.doc
-# think about generic data picker for homogenous
-# just print histogram
 if "__main__" == __name__:
     os.system("python3 sources/main.py -hh")
